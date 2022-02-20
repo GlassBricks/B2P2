8711a09d5def7a6bd07e5eab22e08e2c92121c8d
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-02-20 13:06:07 -0800

    Allow pair to handle negative numbers

--- src/utility/pair-test.ts
+++ src/utility/pair-test.ts
@@ -2,13 +2,14 @@ import { pair, unpair } from "./number-pair"
 
 test("szudzik pair", () => {
   const existing = new LuaSet<number>()
-  for (const x of $range(0, 10)) {
-    for (const y of $range(0, 10)) {
+  for (const x of $range(-10, 10)) {
+    for (const y of $range(-10, 10)) {
       const z = pair(x, y)
       const [x1, y1] = unpair(z)
       assert.equal(x, x1)
       assert.equal(y, y1)
       assert.is_false(existing.has(z))
+      assert.is_true(z >= 0)
 
       existing.add(z, true)
     }
