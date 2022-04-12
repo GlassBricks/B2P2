6389f96280b59be424779450bf391439e2a2af8c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-04-11 20:56:27 -0700

    Fix assertBlueprintsEquivalent

--- src/test/blueprint.ts
+++ src/test/blueprint.ts
@@ -5,5 +5,5 @@ export function assertBlueprintsEquivalent(blueprint1: Blueprint, blueprint2: Bl
   const bp2 = MutableBlueprint.copyOf(blueprint2)
   bp1.sortEntities()
   bp2.sortEntities()
-  assert.same(bp1, bp2)
+  assert.same(bp1.entities, bp2.entities)
 }
