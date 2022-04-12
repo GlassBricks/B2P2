1a166c613dc59194c5008fd0e79f712717330bf4
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-04-11 17:42:53 -0700

    add findOverlappingEntity

--- src/blueprint/blueprint-diff-test.ts
+++ src/blueprint/blueprint-diff-test.ts
@@ -1,14 +1,15 @@
 import { MutableBlueprint } from "./Blueprint"
 import { getEntitySample } from "../test/entity-sample"
 import { createEntity } from "../entity/entity"
-import { findCompatibleEntity } from "./blueprint-diff"
+import { findCompatibleEntity, findOverlappingEntity } from "./blueprint-diff"
+import { pos } from "../lib/geometry/position"
+
+let b: MutableBlueprint
+before_each(() => {
+  b = new MutableBlueprint()
+})
 
 describe("findCompatibleEntity", () => {
-  let b: MutableBlueprint
-  before_each(() => {
-    b = new MutableBlueprint()
-  })
-
   it("Finds compatible entity", () => {
     const rawEntity = getEntitySample("assembling-machine-1")
     const rawEntity2 = {
@@ -18,7 +19,7 @@ describe("findCompatibleEntity", () => {
     const entity = createEntity(rawEntity)
     b.addSingle(entity)
     const result = findCompatibleEntity(b, rawEntity2)
-    assert.equal(result, entity)
+    assert.equal(entity, result)
   })
 
   it("returns undefined if no compatible entity is found", () => {
@@ -27,6 +28,33 @@ describe("findCompatibleEntity", () => {
     const entity = createEntity(rawEntity1)
     b.addSingle(entity)
     const result = findCompatibleEntity(b, rawEntity2)
+    assert.is_nil(result)
+  })
+})
+
+describe("findOverlappingEntity", () => {
+  it("returns overlapping entity", () => {
+    const entity = createEntity(getEntitySample("assembling-machine-1"))
+    const entity2 = createEntity({
+      ...getEntitySample("assembling-machine-2"),
+      position: pos.add(entity.position, pos(1, 0)),
+    })
+
+    b.addSingle(entity)
+
+    const result = findOverlappingEntity(b, entity2)
+    assert.equal(entity, result)
+  })
+  it("returns undefined when overlapping entity not found", () => {
+    const entity = createEntity(getEntitySample("assembling-machine-1"))
+    const entity2 = createEntity({
+      ...getEntitySample("assembling-machine-2"),
+      position: pos.add(entity.position, pos(3, 3)),
+    })
+
+    b.addSingle(entity)
+
+    const result = findOverlappingEntity(b, entity2)
     assert.is_nil(result)
   })
 })
--- src/blueprint/blueprint-diff.ts
+++ src/blueprint/blueprint-diff.ts
@@ -1,6 +1,7 @@
 import { Entity } from "../entity/entity"
 import { Blueprint } from "./Blueprint"
 import { isCompatibleEntity } from "../entity/entity-diff"
+import { bbox } from "../lib/geometry/bounding-box"
 
 export function findCompatibleEntity<T extends Entity>(
   blueprint: Blueprint<T>,
@@ -10,6 +11,18 @@ export function findCompatibleEntity<T extends Entity>(
   if (entities === undefined) return undefined
   for (const [e] of entities) {
     if (isCompatibleEntity(e, entity)) return e
+  }
+  return undefined
+}
+
+// may report compatible entities as overlapping
+export function findOverlappingEntity<T extends Entity>(blueprint: Blueprint<T>, entity: Entity): T | undefined {
+  for (const [x, y] of bbox.iterateTiles(entity.tileBox)) {
+    const entities = blueprint.getAtPos(x, y)
+    if (entities !== undefined) {
+      const entity = entities.first()
+      if (entity !== undefined) return entity
+    }
   }
   return undefined
 }
